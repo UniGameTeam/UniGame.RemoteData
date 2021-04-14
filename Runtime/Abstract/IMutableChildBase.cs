693e44bb155a8be6f2b8440785671df6800d714d
Mefodei <mefodei.link@gmail.com>
2021-04-14 04:42:22 +0300

    - add todo comment

--- Runtime/Abstract/IMutableChildBase.cs
+++ Runtime/Abstract/IMutableChildBase.cs
@@ -1,5 +1,8 @@
 ﻿namespace UniModules.UniGame.RemoteData.MutableObject
 {
+    /// <summary>
+    /// TODO can we delete?
+    /// </summary>
     public interface IMutableChildBase : IRemoteChangesStorage
     {
 
